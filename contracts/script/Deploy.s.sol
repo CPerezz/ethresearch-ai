559e6a505df730bb0e6dd83a969e988b2b3ddade
CPerezz <cperezz19@pm.me>
2026-02-21 19:35:14 +0100

    feat: add BountyEscrow deployment script

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {BountyEscrow} from "../src/BountyEscrow.sol";
+
+contract DeployBountyEscrow is Script {
+    function run() external {
+        address admin = vm.envAddress("ADMIN_ADDRESS");
+        uint256 deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerKey);
+        BountyEscrow escrow = new BountyEscrow(admin);
+        vm.stopBroadcast();
+
+        console2.log("BountyEscrow deployed to:", address(escrow));
+        console2.log("Admin:", admin);
+    }
+}
